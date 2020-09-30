2906c7f53b87dc177d4bfef4b8f63e506eba4ce6
Nikias Hess <nikiasphess@gmail.com>
2020-09-30 16:27:05 +0200

    version 0.8

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import moment from 'moment';
 function App() {
 
   // API Proxy to prevent CORS error
-  const Proxy = 'https://cors-anywhere.herokuapp.com/'
+  //const Proxy = 'https://cors-anywhere.herokuapp.com/'
+  const Proxy = 'https://thingproxy.freeboard.io/fetch/'
   // Station ID of Dornbusch / refer to RMV API documentation for different station
   const stationID_dornbusch = '3001301'
   const stationID_hauptfriedhof= '3001401'
@@ -20,28 +21,23 @@ function App() {
 
   // Dornbusch subway station
   const [u1n, setU1n] = useState([])
-  const [u1s, setU1s] = useState([])
   const u1n_dir = "Frankfurt (Main) Ginnheim"
-  const u1s_dir = "Frankfurt (Main) Südbahnhof"
   const colU1 = "#a93634"
 
   const [u2n, setU2n] = useState([])
-  const [u2s, setU2s] = useState([])
   const u2n_dir = "Bad Homburg v.d.H.-Gonzenheim (U)"
-  const u2s_dir = "Frankfurt (Main) Südbahnhof"
   const colU2 = "#49a258"
 
   const [u3n, setU3n] = useState([])
-  const [u3s, setU3s] = useState([])
   const u3n_dir = "Oberursel (Taunus)-Hohemark"
-  const u3s_dir = "Frankfurt (Main) Südbahnhof"
   const colU3 = "#4d5ea4"
 
   const [u8n, setU8n] = useState([])
-  const [u8s, setU8s] = useState([])
   const u8n_dir = "Frankfurt (Main) Riedberg"
-  const u8s_dir = "Frankfurt (Main) Südbahnhof"
   const colU8 = "#bd81b1"
+
+  const [u1238s, setU1238s] = useState([])
+  const u1238s_dir = "Frankfurt (Main) Südbahnhof"
 
   // Hauptfriedhof subway station
   const [u5n, setU5n] = useState([])
@@ -57,29 +53,30 @@ function App() {
   const b34w_dir = "Frankfurt (Main) Mönchhofstraße"
   const colB34 = "#49a38d"
 
-
-
   useEffect(() => {
 
     //console.log(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID +'&format=json')
+    
 
     // run once, then every x seconds
     axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_dornbusch +'&format=json')
        .then(response => update(response.data.Departure))
 
-    //axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_hauptfriedhof +'&format=json')
-       //.then(response => update_Hauptfriedhof(response.data.Departure))
+    axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_hauptfriedhof +'&format=json')
+       .then(response => update_Hauptfriedhof(response.data.Departure))
 
     const interval = setInterval(() => {
-        axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_dornbusch +'&format=json')
-       .then(response => update(response.data.Departure))
-
-       //axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_hauptfriedhof +'&format=json')
-       //.then(response => update_Hauptfriedhof(response.data.Departure))
-    
+      
       setTime(moment().format('HH:mm'))
-       
-    }, 30000); 
+
+
+        axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_dornbusch +'&format=json')
+        .then(response => update(response.data.Departure))
+
+        axios.get(Proxy + 'https://www.rmv.de/hapi/departureBoard?accessId='+ apiKeys.rmvApiKey +'&id='+ stationID_hauptfriedhof +'&format=json')
+        .then(response => update_Hauptfriedhof(response.data.Departure))
+
+    }, 20000); 
     
         
 
@@ -93,19 +90,17 @@ const update = (data) => {
   //setDepartData(data)
 
   //console.log(data)
-  console.log('update')
+  console.log('update Dornbusch')
+
+  setU1238s(data.filter(function(d) { return (d.direction === u1238s_dir) }))
 
   setU1n(data.filter(function(d) { return (d.Product.line === 'U1' && d.direction === u1n_dir) }))
-  setU1s(data.filter(function(d) { return (d.Product.line === 'U1' && d.direction === u1s_dir) }))
 
   setU2n(data.filter(function(d) { return (d.Product.line === 'U2' && d.direction === u2n_dir) }))
-  setU2s(data.filter(function(d) { return (d.Product.line === 'U2' && d.direction === u2s_dir) }))
 
   setU3n(data.filter(function(d) { return (d.Product.line === 'U3' && d.direction === u3n_dir) }))
-  setU3s(data.filter(function(d) { return (d.Product.line === 'U3' && d.direction === u3s_dir) }))
 
   setU8n(data.filter(function(d) { return (d.Product.line === 'U8' && d.direction === u8n_dir) }))
-  setU8s(data.filter(function(d) { return (d.Product.line === 'U8' && d.direction === u8s_dir) }))
 
   setB34e(data.filter(function(d) { return (d.Product.line === '34' && d.direction === b34e_dir) }))
   setB34w(data.filter(function(d) { return (d.Product.line === '34' && d.direction === b34w_dir) }))
@@ -115,6 +110,8 @@ const update = (data) => {
 const update_Hauptfriedhof = (data) => {
 
   //console.log(data)
+
+  console.log('update Hauptfriedhof')
 
   setU5n(data.filter(function(d) { return (d.Product.line === 'U5' && d.direction === u5n_dir) }))
   setU5s(data.filter(function(d) { return (d.Product.line === 'U5' && d.direction === u5s_dir) }))
@@ -131,26 +128,25 @@ const update_Hauptfriedhof = (data) => {
         </div>
 
         <div className="Tables">
-          <DepTable Title={'U1 Südbahnhof'} Data={u1s} Color={colU1} key={'1'}/>
-          <DepTable Title={'U2 Südbahnhof'} Data={u2s} Color={colU2} key={'2'}/>
-          <DepTable Title={'U3 Südbahnhof'} Data={u3s} Color={colU3} key={'3'}/>
-          <DepTable Title={'U8 Südbahnhof'} Data={u8s} Color={colU8} key={'4'}/>
-          <DepTable Title={'34 Rödelheim'} Data={b34w} Color={colB34} key={'5'}/>
-          <DepTable Title={'U5 Hauptbahnhof'} Data={u5s} Color={colU5} key={'11'}/>
+          <DepTable Title={'Südbahnhof'} Data={u1238s} key={'1'}/>
+          <DepTable Title={'Ginnheim'} Data={u1n} Color={colU1} key={'2'}/>
+          <DepTable Title={'Gonzenheim'} Data={u2n} Color={colU2} key={'3'}/>
+          <DepTable Title={'Hohe Mark'} Data={u3n} Color={colU3} key={'4'}/>
+          <DepTable Title={'Riedberg'} Data={u8n} Color={colU8} key={'5'}/>
         </div>
-        <div className="Tables">
-          <DepTable Title={'U1 Ginnheim'} Data={u1n} Color={colU1} key={'6'}/>
-          <DepTable Title={'U2 Gonzenheim'} Data={u2n} Color={colU2} key={'7'}/>
-          <DepTable Title={'U3 Hohe Mark'} Data={u3n} Color={colU3} key={'8'}/>
-          <DepTable Title={'U8 Riedberg'} Data={u8n} Color={colU8} key={'9'}/>
-          <DepTable Title={'34 Bornheim'} Data={b34e} Color={colB34} key={'10'}/>
-          <DepTable Title={'U5 Preungesheim'} Data={u5n} Color={colU5} key={'12'}/>
+        <div className="Tables">     
+          <DepTable Title={'Rödelheim'} Data={b34w} Color={colB34} key={'6'}/>
+          <DepTable Title={'Bornheim'} Data={b34e} Color={colB34} key={'7'}/>
+          <DepTable Title={'Hauptbahnhof'} Data={u5s} Color={colU5} key={'8'}/>
+          <DepTable Title={'Preungesheim'} Data={u5n} Color={colU5} key={'9'}/>
         </div>
 
         
           
 
       </header>
+
+      
     </div>
   );
 }
--- src/DepTable.js
+++ src/DepTable.js
@@ -6,22 +6,30 @@ import Divider from '@material-ui/core/Divider';
 
 export default function DepTable({ Title, Data, Color}) {
 
+  const colU1 = "#a93634"
+  const colU2 = "#49a258"
+  const colU3 = "#4d5ea4"
+  const colU8 = "#bd81b1"
+  const colU5 = "#347647"
+  const colRMV = "#49a38d"
+
   const TableContainer = {
-    width: 160, 
+    width: 130, 
     height: 250,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'rgba(52, 52, 52, 0)',
     borderRadius: 10,
-    border: "3px solid " + (Color === undefined ? '#a93634' : Color),
+    border: "3px solid " + (Color === undefined ? colRMV : Color),
     margin: 10,
   }
 
   const HeaderContainer = {
-    backgroundColor: Color === undefined ? '#a93634' : Color,
+    backgroundColor: Color === undefined ? colRMV : Color,
     padding: 10,
     fontWeight: 'bold',
     fontSize: 16,
+    textAlign: 'center',
   }
   const BodyContainer = {
     padding: 10,
@@ -29,6 +37,8 @@ export default function DepTable({ Title, Data, Color}) {
   }
   const TextContainer = {
     flexDirection: 'row',
+    marginBottom: 3,
+    marginTop: 2,
   }
   const Text1 = {
     fontWeight: 'bold',
@@ -39,7 +49,57 @@ export default function DepTable({ Title, Data, Color}) {
   }
   const Text3 = {
     fontSize: 10,
-    color: Color === undefined ? '#a93634' : Color
+    color: '#ff0000',
+  }
+
+  const ubahnIcon = (color, line) => (
+    <span style={{
+      backgroundColor: color,
+      borderRadius: 2,
+      padding: 2,
+      fontWeight: 'bold',
+      fontSize: 12,}}>
+    {line}
+  </span>
+  )
+
+  const depTag = (time, rtTime) => (
+    
+    <label>
+
+      <span style={Text1}>
+        {' in ' + moment(rtTime, 'HH:mm:ss').diff(moment(), 'minutes') + ' Minuten'}
+      </span>
+      <br />
+      <span style={Text2}>
+          {"" + rtTime}
+      </span>
+      <span style={Text3}>
+          {time === rtTime ? '' : (' +' + moment(rtTime, 'HH:mm:ss').diff(moment(time, 'HH:mm:ss'), 'minutes'))}
+      </span>
+
+      
+    </label>
+
+  )
+
+  
+
+  const chooseColor = (line) => {
+    switch(line) {
+      case 'U1':
+        return colU1;
+      case 'U2':
+        return colU2;
+      case 'U3':
+        return colU3;
+      case 'U5':
+        return colU5;
+      case 'U8':
+        return colU8;
+      default:
+        return colRMV;
+    }
   }
 
   return (
@@ -58,19 +118,9 @@ export default function DepTable({ Title, Data, Color}) {
 
                             element.rtTime === undefined ? null : 
                             <div key={element.JourneyDetailRef.ref}>
-                                <span style={Text1}>
-                                    {'in ' + moment(element.rtTime, 'HH:mm:ss').diff(moment(), 'minutes') + ' Minuten'}
-                                </span>
-
                                 <div style={TextContainer}>
-
-                                    <span style={Text2}>
-                                        {element.rtTime}
-                                    </span>
-
-                                    <span style={Text3}>
-                                        {element.time === element.rtTime ? '' : (' +' + moment(element.rtTime, 'HH:mm:ss').diff(moment(element.time, 'HH:mm:ss'), 'minutes'))}
-                                    </span>
+                                    {ubahnIcon(chooseColor(element.Product.line), element.Product.line)}
+                                    {depTag(element.time, element.rtTime)}
                                 </div>
 
                                 <Divider/>
